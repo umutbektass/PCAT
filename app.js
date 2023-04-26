55f145921ccfb6b878235ea673a6b2c19b364403
Umut Bektaş <umutbektass.78@hotmail.com>
2023-04-26 17:10:55 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const fn = () => {
-  console.log("HELLO");
-};
+const express = require("express");
+const app = express();
+const path = require("path")
+app.use(express.static("public"))
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
 
 
+
+
+const port = 3002;
+app.listen(port, () => {
+  console.log(`NodeJs ${port} da çalışıyor.`);
+});
